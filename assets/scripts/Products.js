8a9b9cfb4602a328f04dee619da9acd3164791cb
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-11-22 18:32:35 +0700

    push constructor of Product

---
+++ assets/scripts/Products.js
@@ -0,0 +1,13 @@
+// create Constructor
+function typeConstructor(id, name) {
+    this.id = id;
+    this.name = name;
+}
+var types = [
+    new typeConstructor("asus", "ASUS"),
+];
+
+
+function createProducts() {
+    
+}
