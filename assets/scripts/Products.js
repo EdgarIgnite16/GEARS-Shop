223548933bdfc0d4be770e39d9f846ad5ec20ed5
EdgarIgnite16 <77919820+EdgarIgnite16@users.noreply.github.com>
2021-11-23 20:38:18 +0700

    update category Product

--- assets/scripts/Products.js
+++ assets/scripts/Products.js
@@ -5,8 +5,45 @@ function typeConstructor(id, name) {
 }
 
 var types = [
-    new typeConstructor("anime", "Anime"),
-    new typeConstructor("artisan", "Artisan"),
-    new typeConstructor("pudding", "Pudding"),
-    new typeConstructor("other", "Other"),
+    new typeConstructor("anime", "Anime keycap"),
+    new typeConstructor("artisan", "Artisan keycap"),
+    new typeConstructor("pudding", "Pudding keycap"),
+    new typeConstructor("other", "Keycap khác"),
+    new typeConstructor("all", "Xem tất cả...."),
 ];
+
+function categoryActive() {
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+    var temp = $$(".product-list--item-link");
+
+    temp.forEach(items => {
+        items.onclick = function() {
+            $(".product-list--item-link.catagory-Active").classList.remove("catagory-Active")
+            this.classList.add('catagory-Active');
+        }
+    })
+}
+
+function CreateSubMenu() {
+    var tempArray = "";
+    for(var i=0;i<types.length;i++) {
+        if(i != types.length - 1) {
+            tempArray += `
+            <li class="product-list--item js-Product-item">
+                <a id=${types[i].id} class="product-list--item-link" href="#">${types[i].name}</a>
+            </li>
+            `;
+        }else {
+            tempArray += `
+            <li class="product-list--item js-Product-item">
+                <a id=${types[i].id} class="product-list--item-link catagory-Active" href="#">${types[i].name}</a>
+            </li>
+            `;
+        }
+    }
+    document.querySelector(".product-list").innerHTML = tempArray;
+}
+
+CreateSubMenu();
+categoryActive();
